42050fa570735a31f42c6fb051ebc5c88ff5776d
Ron <onedarksky@icloud.com>
2021-11-11 14:44:41 +0800

    Initial Commit

---
+++ BuyItOrNotApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BuyItOrNotApp
+//
+//  Created by Jiang Yung Tse on 2021/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
